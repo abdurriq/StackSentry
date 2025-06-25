f39d95843d2272b166004eef2c27a41872239eaf
Abdurrahmaan Iqbal <abdurriq@amazon.com>
2025-06-25 23:07:34 +0100

    Add migrate step

--- run_server.sh
+++ run_server.sh
@@ -1,1 +1,1 @@
-python3 -m venv .venv && source .venv/bin/activate && pip install -r requirements.txt && python3 manage.py runserver 0.0.0.0:8000
+python3 -m venv .venv && source .venv/bin/activate && pip install -r requirements.txt && python3 manage.py migrate && python3 manage.py runserver 0.0.0.0:8000
